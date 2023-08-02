9ea73cb2e529537f35426dcd3b8ca0a6ea043ef5
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-08-02 08:17:22 +0300

    adding cors

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 8080;
+var cors = require('cors')
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({ origin: 'http://localhost:3000' }))
 
 connectionToENV();
 global.db = require('./utils/connectionDB');
