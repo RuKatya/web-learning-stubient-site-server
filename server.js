357e5be4b671d8a6cbecc18518ec5491b465072b
MaximI <makcimka1477@yandex.ru>
2023-08-02 11:07:22 +0300

    added cors; fixed typo;

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const { connectionToENV } = require('./utils/connectionToENV');
-const express = require('express')
-const app = express()
+const express = require('express');
 const cookieParser = require('cookie-parser');
-const PORT = process.env.PORT || 8080;
+const cors = require('cors');
 
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'localhost';
+const CORS_OPTIONS = {
+    origin: process.env.CORS_URL || 3000,
+};
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(CORS_OPTIONS));
 
 connectionToENV();
 global.db = require('./utils/connectionDB');
 
-app.use('/auth', require('./routers/auth/auth'))
+app.use('/auth', require('./routers/auth/auth'));
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
-})
+    console.log(`http://${HOST}:${PORT}`);
+});
