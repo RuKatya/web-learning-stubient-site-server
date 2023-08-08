bc0e6f3526231463cc6563652d6e31e5aa166578
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-08-08 14:36:18 +0300

    user logout controller

--- routers/user/user.router.js
+++ routers/user/user.router.js
@@ -1,6 +1,9 @@
+const { userLogout } = require('../../controllers/user/user.controller');
+
 const router = require('express').Router();
 
 router
+    .get('/user-logout', userLogout)
 
 
 module.exports = router;
