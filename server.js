9d93529b56d79586dc4d0d8f40fd5531246c3471
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-08-08 19:44:44 +0300

    server error

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-// <<<<<<< cors-branch
 const HOST = process.env.HOST || 'localhost';
 const CORS_OPTIONS = {
     origin: process.env.CORS_URL || 3000,
@@ -15,19 +14,21 @@ const CORS_OPTIONS = {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(CORS_OPTIONS));
-// =======
-// var cors = require('cors')
 
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use(cors({ origin: 'http://localhost:3000' }))
-// >>>>>>> main
+app.set('view engine', 'ejs') //connecting ejs
+console.log(app.get('view engine'))
+// app.set('views', path.resolve(__dirname, 'ejs'))
 
 connectionToENV();
 global.db = require('./utils/connectionDB');
 
 app.use('/auth', require('./routers/auth/auth.router'));
 app.use('/user', require('./routers/user/user.router'));
+
+// Handle 500
+app.use(function (error, req, res, next) {
+    res.send('500: Internal Server Error', 500);
+});
 
 app.listen(PORT, () => {
     console.log(`http://${HOST}:${PORT}`);
